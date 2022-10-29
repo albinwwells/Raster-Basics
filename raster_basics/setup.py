31b341b41158bdd39e665f07243c12e30060fdbe
albinwwells <91861031+albinwwells@users.noreply.github.com>
2022-10-29 11:55:51 -0400

    Add files via upload

---
+++ raster_basics/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='raster_basics',
+      version='0.0',
+      description='Basic GeoTIFF Processing',
+      packages=['raster_basics'],
+      author_email='awwells@andrew.cmu.edu',
+      zip_safe=False)
