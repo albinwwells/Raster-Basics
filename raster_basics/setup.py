9c07d1da5b1b73da16163b04f9d268e7b7bd6960
albinwwells <91861031+albinwwells@users.noreply.github.com>
2022-11-11 11:21:54 -0500

    Delete raster_basics directory

--- raster_basics/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='raster_basics',
-      version='0.0',
-      description='Basic GeoTIFF Processing',
-      packages=['raster_basics'],
-      author_email='awwells@andrew.cmu.edu',
-      zip_safe=False)
